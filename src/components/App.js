52bac95546773391fcd2c670beed0a925e33697a
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-04-09 10:07:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import '../styles/App.css';
 
-const posts==(category)=>{
+const posts= (category)=>{
   const API_KEY="a1741280db6dbc554c889a31544851b7"
   let url=`https://gnews.io/api/v4/top-headlines?category=${category}&apikey=${API_KEY}&max=10&lang=en`;
   return fetch(url)
 }
 
 const App = () => {
+  
   const [category, setCategory] = useState("general");
   const [newsData, setNewsData] = useState([]);
-  const [loading, setLoading] = useState();
-    
+  const [loading, setLoading] = useState(true);
+
   useEffect(()=>{
     setLoading(true)
     posts(category)
@@ -24,7 +25,7 @@ const App = () => {
     })
     console.log(newsData)
   },[category])
-  
+
   return (
     <div id="main">
       <h1 className='heading'>Top 10 {category} news.</h1>
@@ -37,23 +38,22 @@ const App = () => {
         <option value="entertainment">Entertainment</option>
         <option value="science">Science</option>
       </select>
-{loading &&  <p className='loader'>Loading...</p>}
- {!loading &&
-     
+      {loading && <p className='loader'>Loading...</p>}
+      {!loading &&
       <ol>
-   {newData.map((item,index)=>{
-     return(
+        {newsData.map((item,index)=>{
+          return(
         <li key={index}>
           <img className='news-img' src={item.image} alt="randomimg"/>
           <section className='new-title-content-author'>
             <h3 className='news-title'>{item.title}</h3>
             <section className='new-content-author'>
               <p className='news-description'>{item.description}</p>
-              <p className='news-source'><strong>Source:</strong> {item.source.name}</p>
+              <p className='news-source'><strong>Source:</strong>{item.source.name}</p>
             </section>
           </section>
         </li>)
- })}
+        })}
       </ol>}
     </div>
   )
